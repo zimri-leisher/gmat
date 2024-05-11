c4320b889921dc7a44c604e865f36a957e4319c4
zimri-leisher <zimri.leisher@gmail.com>
2024-05-10 20:06:35 -0500

    Convert into runnable script with arguments

--- simulate.py
+++ simulate.py
@@ -1,83 +1,117 @@
+import plotly
+import plotly.graph_objects as go
+from types import ModuleType
+from argparse import ArgumentParser
+from dataclasses import dataclass
 from pathlib import Path
 import sys
 
-gmat_install = Path("/workspaces/gmat/R2022a")
 
-api_startup_file_name = "api_startup_file.txt"
-gmat_bin = gmat_install / "bin"
-startup_file = gmat_bin / api_startup_file_name
+@dataclass
+class Scenario:
+    script: Path
+    crew_count: int
+    crew_average_mass: float  # kg
+    spacecraft_dry_mass: int  # kg
 
-if startup_file.exists():
-    sys.path.insert(1, str(gmat_bin.resolve()))
 
-    import gmatpy as gmat
+@dataclass
+class ScenarioOutput:
+    mass_timeseries: list[tuple[float, float]]
 
-    gmat.Setup(str(startup_file))
+    def plot(self):
+        x = [t[0] for t in self.mass_timeseries]
+        y = [t[1] for t in self.mass_timeseries]
+        fig = go.Figure()
+        fig.update_layout(title_text="Mass over time of crewed spacecraft to Luna")
+        fig.update_xaxes(title_text="Time (s)")
+        fig.update_yaxes(title_text="Mass (kg)")
+        fig.add_trace(go.Line(x=x, y=y))
+        fig.show()
 
-else:
-    print("Cannot find GMAT api_startup_file.txt")
-    exit(1)
 
-# Use a core script file to drive the analysis
-script = gmat_install / "api" / "Ex_R2020a_ToLuna.script"
-gmat.LoadScript(str(script.resolve()))
+def load_gmat() -> ModuleType:
+    gmat_install = Path("/workspaces/gmat/R2022a")
 
-# Set up the objects used to scan for a solution
-burn = gmat.GetObject("TOI")
-time = gmat.GetObject("LeoTime")
-start = gmat.GetObject("StartEpoch")
-tank = gmat.GetObject("FuelTank1")
+    api_startup_file_name = "api_startup_file.txt"
+    gmat_bin = gmat_install / "bin"
+    startup_file = gmat_bin / api_startup_file_name
 
-print()
+    if startup_file.exists():
+        sys.path.insert(1, str(gmat_bin.resolve()))
 
-closest = 1000000000.0
-dt = 4000
-dv = 2.9
-start_time = 0.0
+        import gmatpy as gmat
 
-# Scan through the delta-V using the burn object
-for i in range(10):
-    # Scan through the cost time in low Earth orbit
-    for j in range(20):
-        # Scan through the epoch of the insertion state into orbit
-        for k in range(25):
-            start_epoch = k / 2.0
-            start.SetField("Value", start_epoch / 24)
-            time.SetField("Value", 1500.0 + i * 100)
-            burn.SetField("Element1", 3.0 + j * 0.01)
-            gmat.RunScript()
-            moon_dist = gmat.GetRuntimeObject("MoonDistance")
-            r = moon_dist.GetNumber("Value")
-            if closest > r or True:
-                closest = r
-                dt = time.GetNumber("Value")
-                dv = burn.GetNumber("Element1")
-                start_time = start.GetNumber("Value")
-                fuel = tank.GetNumber("FuelMass")
+        gmat.Setup(str(startup_file))
 
-                # Report intermediate results if they are better than previous values
-                print(
-                    closest,
-                    "Launch ",
-                    start_time,
-                    " Coast time: ",
-                    dt,
-                    "   Delta-V: ",
-                    dv,
-                    "   Moon Distance: ",
-                    r,
-                    " Fuel Mass: ",
-                    fuel
-                )
+        return gmat
 
-# Build the name for the solution script
-soln = script.with_stem(script.stem + "_soln")
-print("\nSaving solution to ", soln, "\n")
+    else:
+        print("Cannot find GMAT api_startup_file.txt")
+        exit(1)
 
-# Set the values for the best solution found
-start.SetField("Value", start_time)
-time.SetField("Value", dt)
-burn.SetField("Element1", dv)
 
-# Write out a script with the best values found
-gmat.SaveScript(str(soln.resolve()))
+def run_scenario(scenario: Scenario) -> ScenarioOutput:
+
+    gmat = load_gmat()
+
+    # Use a core script file to drive the analysis
+    gmat.LoadScript(str(scenario.script.resolve()))
+
+    # Set up the objects used to scan for a solution
+    sat = gmat.GetObject("Sat")
+    dry_mass = float(sat.GetField("DryMass"))
+    dry_mass += scenario.crew_average_mass * scenario.crew_count
+
+    sat.SetField("DryMass", dry_mass)
+
+    mass_timeseries = [(0, dry_mass)]
+
+    gmat.RunScript()
+
+    tank = gmat.GetObject("FuelTank1")
+    fuel_mass = tank.GetField("FuelMass")
+
+    # TODO... figure out how to advance the simulation in steps and get fuel mass
+
+    final_time = sat.GetField("ElapsedSecs")
+
+    mass_timeseries.append([(final_time, fuel_mass)])
+
+    return ScenarioOutput(mass_timeseries)
+
+
+def main():
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument(
+        "--crew_count",
+        "-c",
+        type=int,
+        default=2,
+        help="The number of crew on the spacecraft",
+    )
+    arg_parser.add_argument(
+        "--script",
+        "-s",
+        type=Path,
+        default="lunar_transfer.script",
+        help="The path to the GMAT script",
+    )
+    arg_parser.add_argument("--crew_avg_mass", type=float, default=62, help="units: kg")
+    arg_parser.add_argument(
+        "--spacecraft_dry_mass", type=float, default=62, help="units: kg"
+    )
+
+    args = arg_parser.parse_args()
+
+    scenario = Scenario(
+        args.script, args.crew_count, args.crew_avg_mass, args.spacecraft_dry_mass
+    )
+
+    output = run_scenario(scenario)
+
+    output.plot()
+
+
+if __name__ == "__main__":
+    main()
